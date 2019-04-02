c5d55cd1bfcd626ad3a36ca8c0ad3b289725740a
andy <andys920605@gmail.com>
2019-04-02 13:36:18 +0800

    done demo

--- mblog/settings.py
+++ mblog/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [    # 加入 static 路徑
+    os.path.join(BASE_DIR, 'static'),
+]
